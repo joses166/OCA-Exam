af015dd9cfb4c9b9706585470974cb3a83aff2f1
unknown <josehamiltonmartinsleite@gmail.com>
2022-03-28 20:42:57 -0300

    exercicio 3-1

---
+++ Capitulo_03/exercicioscapitulo03/src/exercicioscapitulo03/Exercicio_3_1.java
@@ -0,0 +1,19 @@
+package exercicioscapitulo03;
+
+public class Exercicio_3_1 {
+
+	public static void main(String[] args) {
+		System.out.println(10 + 3);
+		System.out.println(15 - 3);
+		System.out.println(20 * 3);
+		System.out.println(25 / 3);
+		System.out.println(30 % 3);
+		System.out.println(35 & 3); // não cai
+		System.out.println(40 ^ 3); // não cai
+		System.out.println(45 | 3); // não cai
+		System.out.println(50 << 3); // não cai
+		System.out.println(55 >> 3); // não cai
+		System.out.println(60 >>> 3); // não cai
+	}
+
+}
